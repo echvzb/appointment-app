01790ba9ecb01f8f67de18f61045f74450727687
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-02-07 11:02:17 -0600

    fix: tailwind css apply css (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './public/index.html',
+    './src/pages/*.{js,jsx,ts,tsx}',
+    './src/components/*.{js,jsx,ts,tsx}',
+  ],
 };
