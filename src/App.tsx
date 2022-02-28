8780a89dc1961d6b3275c359bf2885466b2269fb
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-02-27 20:36:32 -0600

    fix: remove styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import Router from './Router';
-import {ThemeProvider} from 'styled-components';
-import {GlobalStyle, theme} from './style';
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Router />
-  </ThemeProvider>
-);
+const App = () => <Router />;
 
 export default App;
