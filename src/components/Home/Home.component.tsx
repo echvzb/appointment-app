97524760aabfd6d0854b0f953a86920681c1711f
Eduardo Chavez Barreto <95049400+educhavezbarreto@users.noreply.github.com>
2022-03-03 22:30:58 -0600

    feat: add simple navbar (web) (#17)
    * feat: add simple web navbar
    
    * fix: decrease avatar size
    
    * fix: remove theme console log
    
    * fix: remove material icons
    
    * fix: remove redundant logic

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,12 +1,3 @@
-import {Button} from 'baseui/button';
-import {useHome} from './Home.hook';
-
 export const Home = () => {
-  const {currentTheme, toggleTheme, buttonText, handleClick} = useHome();
-  return (
-    <div>
-      <Button onClick={toggleTheme}>{currentTheme}</Button>
-      <Button onClick={handleClick}>{buttonText}</Button>
-    </div>
-  );
+  return <div>Hello, world!</div>;
 };
--- src/components/Navbar/Navbar.hook.ts
+++ src/components/Navbar/Navbar.hook.ts
@@ -2,7 +2,7 @@ import {useAtom} from 'jotai';
 import {currentThemeAtom, userAtom, setUserAtom, tokenAtom} from '../../state';
 import {useNavigate} from 'react-router-dom';
 
-export const useHome = () => {
+export const useNavbar = () => {
   const [currentTheme, setCurrentTheme] = useAtom(currentThemeAtom);
   const [, setUser] = useAtom(setUserAtom);
   const [user] = useAtom(userAtom);
@@ -20,6 +20,8 @@ export const useHome = () => {
 
   if (user.isAuthenticated) {
     return {
+      userName: user.name,
+      image: user.image,
       currentTheme,
       toggleTheme,
       buttonText: 'Logout',
@@ -27,6 +29,8 @@ export const useHome = () => {
     };
   }
   return {
+    userName: '',
+    image: '',
     currentTheme,
     toggleTheme,
     buttonText: 'Login',
